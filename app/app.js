12201c013d4d07a1c56312ffa4ad226a09b08d30
Steve Lohr <schdief.law@gmail.com>
2018-01-31 09:44:53 +0000

    set up basic express app

---
+++ app/app.js
@@ -0,0 +1,18 @@
+var express = require("express"),
+    app = express()
+    
+//VIEW
+app.set("view engine", "ejs")
+app.use(express.static(__dirname + "/views/public"))
+
+//ROUTES
+//index
+app.get("/", (req, res) => res.render("index"))
+
+//SERVER
+//PORT is defined by environment variable or 80
+const PORT = process.env.PORT || 80
+const HOST = '0.0.0.0'
+app.listen(PORT, HOST, () => {
+    console.log("quashy started on " + HOST + ":" + PORT)
+})
